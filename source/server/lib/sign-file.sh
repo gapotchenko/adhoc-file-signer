e792bd8f92313901c54926d2ba2e64570410a2b2
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2025-09-21 00:34:12 +0200

    In progress.

--- source/server/lib/sign-file.sh
+++ source/server/lib/sign-file.sh
@@ -3,15 +3,17 @@
 # Your feedback and contributions are welcome:
 # https://github.com/gapotchenko/adhoc-file-signer
 
+set -eu
+
 SCRIPT_DIR="$(dirname "$(readlink -fn -- "$0")")"
 BASE_DIR="$(dirname "$SCRIPT_DIR")"
 
 # Enusre that the tools we depend on are in PATH.
 PATH="$PATH:$BASE_DIR/usr/bin"
 
-signtool
-
 echo TODO
-echo "File being processed: $1"
 
+# signtool
+
+echo "File being processed: $1"
 echo "Addendum" >>"$1"
