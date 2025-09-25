0d7c7f5eaf404dd39be8fad6fd78171cf9f30bce
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2025-09-25 11:22:08 +0200

    server: formalized bin/run.sh script.

--- source/server/bin/run.sh
+++ source/server/bin/run.sh
@@ -3,10 +3,85 @@
 set -eu
 
 # -----------------------------------------------------------------------------
+# Help
+# -----------------------------------------------------------------------------
+
+NAME=run.sh
+VERSION=0.0.0
+
+help() {
+    echo "adhoc-file-signer/server  Version $VERSION
+Copyright © Gapotchenko and Contributors
+
+Runs the 'adhoc-file-signer' server.
+
+Usage:
+  $NAME [--host <server-host>] [--port <server-port>]
+
+Options:
+  --host  The server host to bind to. Defaults to 'localhost'.
+  --port  The server port to listen at. Defaults to 3205."
+}
+
+# -----------------------------------------------------------------------------
 # Options
 # -----------------------------------------------------------------------------
 
-# TODO
+OPT_HOST=localhost
+OPT_PORT=3205
+
+# Parse options
+while [ $# -gt 0 ]; do
+    case "$1" in
+    --help)
+        help
+        exit
+        ;;
+    --host)
+        OPT_HOST=$2
+        shift 2
+        ;;
+    --port)
+        OPT_PORT=$2
+        shift 2
+        ;;
+    *)
+        echo "$NAME: unknown option: $1" >&2
+        exit 2
+        ;;
+    esac
+done
+
+# Validate options
+
+opt_error_not_specified() {
+    echo "$NAME: $1 not specified." >&2
+    exit 2
+}
+
+if [ -z "$OPT_HOST" ]; then
+    opt_error_not_specified 'server host'
+fi
+
+if [ -z "$OPT_PORT" ]; then
+    opt_error_not_specified 'server port'
+fi
+
+# -----------------------------------------------------------------------------
+# Functions
+# -----------------------------------------------------------------------------
+
+log() {
+    echo "$@"
+}
+
+error() {
+    echo "$@" >&2
+}
+
+get_file_extension() {
+    expr "x$1" : '.*\.\([^.]*\)$' | tr '[:upper:]' '[:lower:]' || true
+}
 
 # -----------------------------------------------------------------------------
 # Core Functionality
@@ -18,8 +93,32 @@ TMP_DIR=${TMPDIR-$(dirname "$(mktemp -u)")}
 
 OS=$(uname -o)
 
-log() {
-    echo "$@"
+validate_configuration() {
+    log "Validating configuration..."
+
+    if [ -z "${GP_ADHOC_FILE_SIGNER_CERTIFICATE_FILE-}" ]; then
+        error "$NAME: GP_ADHOC_FILE_SIGNER_CERTIFICATE_FILE environment variable is not set."
+        exit 2
+    fi
+
+    if [ -z "${GP_ADHOC_FILE_SIGNER_CERTIFICATE_PASSWORD-}" ]; then
+        case $(get_file_extension "${GP_ADHOC_FILE_SIGNER_CERTIFICATE_FILE-}") in
+        pfx | p12)
+            error "$NAME: GP_ADHOC_FILE_SIGNER_CERTIFICATE_FILE environment variable specifies a .pfx/.p12 certificate file, but GP_ADHOC_FILE_SIGNER_CERTIFICATE_PASSWORD is not set."
+            exit 2
+            ;;
+        esac
+    fi
+
+    if [ -z "${GP_ADHOC_FILE_SIGNER_FILE_DIGEST-}" ]; then
+        error "$NAME: GP_ADHOC_FILE_SIGNER_FILE_DIGEST environment variable is not set."
+        exit 2
+    fi
+
+    if [ -n "${GP_ADHOC_FILE_SIGNER_TIMESTAMP_SERVER-}" ] && [ -z "${GP_ADHOC_FILE_SIGNER_TIMESTAMP_DIGEST-}" ]; then
+        error "$NAME: GP_ADHOC_FILE_SIGNER_TIMESTAMP_SERVER environment variable specifies a timestamp server URL, but GP_ADHOC_FILE_SIGNER_TIMESTAMP_DIGEST is not set."
+        exit 2
+    fi
 }
 
 initialize_host() {
@@ -65,6 +164,16 @@ initialize_hsm() {
     log "HSM: initialization done."
 }
 
+run_http() {
+    log "Starting HTTP server..."
+    exec "$BASE_DIR/srv/deno/run.sh" --host "$OPT_HOST" --port "$OPT_PORT" 2>&1
+}
+
+# -----------------------------------------------------------------------------
+
+log "adhoc-file-signer/server  Version $VERSION"
+
+validate_configuration
 initialize_host
 
 if [ -n "${GP_ADHOC_FILE_SIGNER_CERTIFICATE_FILE-}" ] &&
@@ -80,5 +189,4 @@ if [ -n "${GP_ADHOC_FILE_SIGNER_CERTIFICATE_FILE-}" ] &&
     log
 fi
 
-log "Starting HTTP server..."
-exec "$BASE_DIR/srv/deno/run.sh" "$@" 2>&1
+run_http
