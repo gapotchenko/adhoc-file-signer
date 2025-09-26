b44fb892676aea8819facd3f84b4d9067cf81a30
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2025-09-26 16:11:13 +0200

    Ensure that zstd actually works.

--- source/server/srv/cgi/cgi-capabilities.sh
+++ source/server/srv/cgi/cgi-capabilities.sh
@@ -4,11 +4,28 @@
 
 set -eu
 
+NAME=cgi-capabilities.sh
+
+# -----------------------------------------------------------------------------
+
+on_exit() {
+    [ -n "${tmpfile-}" ] && rm -f "$tmpfile" || true
+}
+
+trap 'on_exit' EXIT
+
+# -----------------------------------------------------------------------------
+
 # Determine which encodings are supported by the server.
 RESPONSE_ACCEPT_ENCODING=gzip
-# The list depends on tools availability.
+# The list depends on tools availability
 if command -v zstd >/dev/null 2>&1; then
-    RESPONSE_ACCEPT_ENCODING="$RESPONSE_ACCEPT_ENCODING, zstd"
+    # Ensure that zstd actually works
+    tmpfile=$(mktemp -t "$NAME.XXXXXX")
+    status=0 && zstd -c "$tmpfile" >/dev/null 2>&1 || status=$?
+    if [ "$status" -eq 0 ]; then
+        RESPONSE_ACCEPT_ENCODING="$RESPONSE_ACCEPT_ENCODING, zstd"
+    fi
 fi
 
 # Start transmitting HTTP response at the very end so that a client can detect
