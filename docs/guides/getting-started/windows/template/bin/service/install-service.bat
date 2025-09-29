9fb929ea0ecdcdd8b43b949540ec0aa2c38bd6c0
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2025-09-29 23:22:58 +0200

    Updated service name in guide's template for Windows.

--- docs/guides/getting-started/windows/template/bin/service/install-service.bat
+++ docs/guides/getting-started/windows/template/bin/service/install-service.bat
@@ -1,4 +1,4 @@
 @echo off
 winsw install
-echo Make sure that "Custom App Server" service is being run using the "AppServer" user account.
-echo Use services.msc applet for that.
+echo Make sure that "Local App Server" service is being run under the "AppServer" user account.
+echo Use services.msc Windows applet for that.
