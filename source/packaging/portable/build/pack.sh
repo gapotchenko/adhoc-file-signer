b9596863cf97b39834f31c86a385d6e34a5e02b1
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2025-09-30 01:08:12 +0200

    Implemented portable packaging.

--- source/packaging/portable/build/pack.sh
+++ source/packaging/portable/build/pack.sh
@@ -11,4 +11,67 @@ VERSION=$(cat "$REPO_PATH/source/mastering/version")
 
 # -----------------------------------------------------------------------------
 
-echo "$VERSION"
+stamp() {
+    sed "s/^VERSION=0\.0\.0\$/VERSION=$VERSION/" "$1" | sponge "$1"
+}
+
+get_output_file_name() {
+    echo "adhoc-file-signer-$VERSION-$1"
+}
+
+# -----------------------------------------------------------------------------
+
+pack_client() {
+    mkdir .obj/client
+
+    cp "$REPO_PATH/source/client/adhoc-sign-tool" .obj/client
+    cp "$REPO_PATH/source/client"/*.bat .obj/client
+    cp "$REPO_PATH/source/client"/*.md .obj/client
+    cp "$REPO_PATH/LICENSE" .obj/client
+    cp "$REPO_PATH/docs/ABOUT" .obj/client
+
+    chmod +x .obj/client/adhoc-sign-tool
+
+    stamp .obj/client/adhoc-sign-tool
+
+    output_file_name="$(get_output_file_name client).tar.gz"
+
+    echo "$output_file_name:"
+    cd .obj/client
+    tar czvf "../../.bin/$output_file_name" -- * | sed 's_^_  adding: _'
+    cd ../..
+}
+
+pack_server() {
+    cp -r "$REPO_PATH/source/server" .obj
+    find .obj/server -type f -name justfile -exec rm {} +
+    find .obj/server -type f -name '*.code-workspace' -exec rm {} +
+
+    cp "$REPO_PATH/LICENSE" .obj/server
+    cp "$REPO_PATH/docs/ABOUT" .obj/server
+
+    find .obj/server -type f -name '*.sh' -exec chmod +x {} +
+    chmod +x .obj/server/bin/adhoc-sign-server .obj/server/usr/bin/signtool
+
+    stamp .obj/server/bin/adhoc-sign-server
+    stamp .obj/server/lib/sign-file.sh
+
+    output_file_name="$(get_output_file_name server).tar.gz"
+
+    echo "$output_file_name:"
+    cd .obj/server
+    tar czvf "../../.bin/$output_file_name" -- * | sed 's_^_  adding: _'
+    cd ../..
+}
+
+# -----------------------------------------------------------------------------
+
+pack_client
+pack_server
+
+# -----------------------------------------------------------------------------
+
+cd .bin
+
+echo "Calculating checksums..."
+sha256sum -b -- * | tr '*' ' ' >SHA256SUMS
