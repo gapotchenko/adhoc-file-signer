d04becc23836aa17a333fe90a6957597e886db00
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2025-10-02 11:35:04 +0200

    Update run.sh
    If this script is terminated, terminate all child processes.

--- docs/guides/getting-started/windows/template/bin/run.sh
+++ docs/guides/getting-started/windows/template/bin/run.sh
@@ -10,6 +10,9 @@ export TERM=dumb
 export NO_COLOR=1
 export PATH="$PATH:$BASE_DIR/usr/bin"
 
+# If this script is terminated, terminate all child processes.
+trap 'trap - INT TERM; kill -TERM -$$ 2>/dev/null; wait' INT TERM
+
 lib/supervise.sh -- tailscale funnel 3205 &
 lib/supervise.sh -- opt/adhoc-file-signer/bin/adhoc-sign-server --host 127.0.0.1 2>&1 &
 
@@ -18,10 +21,12 @@ MS/Windows)
     # 'wait' causes CPU spinning in BusyBox for Windows.
     # Using an endless loop instead as a workaround.
     while :; do
-        sleep 30
+        # Do not sleep for too long to be able to receive signals soon enough
+        sleep 1
     done
     ;;
 *)
+    # Wait for background processes to finish.
     wait
     ;;
 esac
